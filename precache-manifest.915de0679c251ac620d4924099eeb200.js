7bccfa9618e37a649948ece53ac5e1fce3a15c52
theghost1980 <saturnob612@gmail.com>
2020-10-07 17:26:53 -0400

    Updates

---
+++ precache-manifest.915de0679c251ac620d4924099eeb200.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63f782cacbfe973f0215a47290130533",
+    "url": "/React1/index.html"
+  },
+  {
+    "revision": "39f4d57c1e008743773d",
+    "url": "/React1/static/css/main.44454656.chunk.css"
+  },
+  {
+    "revision": "072cf5cdd19157025652",
+    "url": "/React1/static/js/2.6b997b08.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React1/static/js/2.6b997b08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39f4d57c1e008743773d",
+    "url": "/React1/static/js/main.0fbf733e.chunk.js"
+  },
+  {
+    "revision": "f079e7aa15d354d0c610",
+    "url": "/React1/static/js/runtime-main.4eb18c88.js"
+  },
+  {
+    "revision": "88823c2015ffd5fa89d567e17297a137",
+    "url": "/React1/static/media/Roboto-Light.88823c20.ttf"
+  },
+  {
+    "revision": "8f227c4513eaf7ce6fe99a5491b01a0f",
+    "url": "/React1/static/media/bg-1.8f227c45.jpg"
+  },
+  {
+    "revision": "0597f736b7345a15373f3fafcf44a10c",
+    "url": "/React1/static/media/guy-programming.0597f736.jpg"
+  },
+  {
+    "revision": "f55fc1a0307b568e56fe33d5b8ab39b3",
+    "url": "/React1/static/media/profile-pic.f55fc1a0.jpeg"
+  }
+]);
