413ebe957999bfe193e5176d818bc89da7069246
theghost1980 <saturnob612@gmail.com>
2020-10-07 16:39:40 -0400

    First commit

---
+++ precache-manifest.eca3cdaae0560b0604f15b20d9e77d8a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fb13ff946b6daecf251f3e64a7168b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e315608d4858b1ea39ae",
+    "url": "/static/css/main.95a538ab.chunk.css"
+  },
+  {
+    "revision": "072cf5cdd19157025652",
+    "url": "/static/js/2.6b997b08.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6b997b08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e315608d4858b1ea39ae",
+    "url": "/static/js/main.8ec7e4ba.chunk.js"
+  },
+  {
+    "revision": "7fa187e5ed8b47704a1b",
+    "url": "/static/js/runtime-main.de45c872.js"
+  },
+  {
+    "revision": "88823c2015ffd5fa89d567e17297a137",
+    "url": "/static/media/Roboto-Light.88823c20.ttf"
+  },
+  {
+    "revision": "8f227c4513eaf7ce6fe99a5491b01a0f",
+    "url": "/static/media/bg-1.8f227c45.jpg"
+  },
+  {
+    "revision": "0597f736b7345a15373f3fafcf44a10c",
+    "url": "/static/media/guy-programming.0597f736.jpg"
+  },
+  {
+    "revision": "f55fc1a0307b568e56fe33d5b8ab39b3",
+    "url": "/static/media/profile-pic.f55fc1a0.jpeg"
+  }
+]);
